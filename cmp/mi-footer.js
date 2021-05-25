6c8e3db49250544246cfd28c20e6920432aa8055
César Ivan Tolentino Garcia <67447064+NorthonRNG@users.noreply.github.com>
2021-05-24 21:28:54 -0500

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; 2021 Tolentino Garcia César Ivan.`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
